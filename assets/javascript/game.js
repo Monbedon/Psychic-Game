65973527795dac3ea53d7dae303bd576e6c423cd
Monbedon <monbedon@gmail.com>
2017-05-13 07:37:55 -0700

    jstrouble

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,36 @@
-var options = ["a","z","c","g","w","p"];
-var winCount = 0;
-var lossCount = 0;
+//array of choices
+var choices = ["a","z","c","g","w","p"];
+
+ //number of wins, losses, guesses and letters selected
+var wins = 0;
+var losses = 0;
 var guessleft = 0;
 
-//when i press a key
+//when i press a key an event will happen
 document.onkeyup = function(event){
+
 	var userGuess = event.key;
 
-if (userGuess === "a") || (userGuess === "z") || (userGuess === "c") || (userGuess === "g") || (userGuess === "w") ||(userGuess === "p"){
-	wins++;
+//if i press these keys then it will add to my winCount
+if ((userGuess === "a") || (userGuess === "z") || (userGuess === "c") || (userGuess === "g") || (userGuess === "w") ||(userGuess === "p")){
+	winCount++;
 }
+// if I press these keys then it will add to my lossCount
+else if((userGuess !== "a") || (userGuess !== "z") || (userGuess !== "c") || (userGuess !== "g") || (userGuess !== "w") ||(userGuess !== "p")){ 
+	lossCount++;
+
+if ((guessleft = 0; guessleft < 5; i++)){
+	console.log(" You have + guessleft ");
 }
+
+}
+	<var html = "<p>The Psychic Game</p>" +
+	"<p> Guess what letter I'm thinking of...</p>"
+	"<p> Wins:" + wins + "</p>" +
+	"<p> Loss:"+ losses + "</p>" +
+	"<p> Guesses Left: " + guessleft + "</p>" +
+	"<p> Your guesses so far: " + guesschoices + "</p>";
+
+	document.querySelector("#game").innerHTML = html;
+        }
+    };
