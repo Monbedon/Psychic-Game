ea3d5867ef26329c534df00832d552ff644cf65a
Monbedon <monbedon@gmail.com>
2017-05-14 13:28:09 -0700

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,61 @@
 
 
 //array of choices
-var choices = ["a","z","c","g","w","p"];
+var choices = ["a","z","c"];
 
  //number of wins, losses, guesses and letters selected
 var wins = 0;
 var losses = 0;
-var guessLeft = 0;
+var guessLeft = 5;
 var guessesSoFar = [];
 
 var letter = choices[Math.floor(Math.random()* choices.length)];
 
 //when i press a key an event will happen
-document.onkeyup = function(event){
+	document.onkeyup = function(event){
 
 	var userGuess = event.key;
 
 	guessesSoFar.push(userGuess)
 
-//if userGuesses a letter, then you either win a point or 
-//lose a point. 
-if((userGuess === "a") && (letter === "a")) {
- 	wins = 0; guessLeft < 5; wins++;
 
-}
+//if userGuess presses a letter, then you either add a win point or 
+// a lose point. 
 
+	if(userGuess === "a"){
+		wins++;guessLeft--;
+	}
+	else if(userGuess !== "a"){ 
+		losses++;guessLeft--;
+	}
 //You have five tries before it resets.
+	if((guessLeft === 0) || (userGuess === "a")){
+		((guessesoFar = []) && (guessLeft = 5));
+	}
 
+// 	if(userGuess === "z"){
+// 		wins++;guessLeft--;
+// 	}
+// 	else if(userGuess !== "z"){ 
+// 		losses++;guessLeft--;
+// 	}
+// //You have five tries before it resets.
+// 	if((guessLeft === 0) || (userGuess === "z")){
+// 		((guessesoFar = []) && (guessLeft = 5));
+// 	}
 
+// 	if(userGuess === "c"){
+// 		wins++;guessLeft--;
+// 	}
+// 	else if(userGuess !== "c"){ 
+// 		losses++;guessLeft--;
+// 	}
+// //You have five tries before it resets.
+// 	if((guessLeft === 0) || (userGuess === "c")){
+// 		((guessesoFar = []) && (guessLeft = 5));
+// 	}
+  		
 // it will keep track of the letter you guesses.
-
-
-
-
 	var html = "<h1>The Psychic Game</h1>" +
 	//"<h2> Guess what letter I'm thinking of...</h2>"
 	"<p> wins:" + wins + "</p>" +
@@ -41,5 +64,8 @@ if((userGuess === "a") && (letter === "a")) {
 	"<p> Your guesses so far: " + guessesSoFar + "</p>";
 
 	document.querySelector("#game").innerHTML = html;
+
 }
+
+
     
