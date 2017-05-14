07c53e4a93246317a2e3aaf7f25716adeffce305
Monbedon <monbedon@gmail.com>
2017-05-13 20:29:15 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,33 +6,40 @@ var choices = ["a","z","c","g","w","p"];
  //number of wins, losses, guesses and letters selected
 var wins = 0;
 var losses = 0;
-var guessleft = 0;
+var guessLeft = 0;
+var guessesSoFar = [];
+
+var letter = choices[Math.floor(Math.random()* choices.length)];
 
 //when i press a key an event will happen
 document.onkeyup = function(event){
 
 	var userGuess = event.key;
 
-//if i press these keys then it will add to my winCount
-if ((userGuess === "a") || (userGuess === "z") || (userGuess === "c") || (userGuess === "g") || (userGuess === "w") ||(userGuess === "p")){
-	winCount++;
-}
-// if I press these keys then it will add to my lossCount
-else if((userGuess !== "a") || (userGuess !== "z") || (userGuess !== "c") || (userGuess !== "g") || (userGuess !== "w") ||(userGuess !== "p")){ 
-	lossCount++;
+	guessesSoFar.push(userGuess)
 
-if ((guessleft = 0; guessleft < 5; i++)){
-	console.log(" You have + guessleft ");
-}
+//if userGuesses a letter, then you either win a point or 
+//lose a point. 
+if((userGuess === "a") && (letter === "a")) {
+ 	wins = 0; guessLeft < 5; wins++;
 
 }
-	<var html = "<p>The Psychic Game</p>" +
-	"<p> Guess what letter I'm thinking of...</p>"
-	"<p> Wins:" + wins + "</p>" +
-	"<p> Loss:"+ losses + "</p>" +
-	"<p> Guesses Left: " + guessleft + "</p>" +
-	"<p> Your guesses so far: " + guesschoices + "</p>";
+
+//You have five tries before it resets.
+
+
+// it will keep track of the letter you guesses.
+
+
+
+
+	var html = "<h1>The Psychic Game</h1>" +
+	//"<h2> Guess what letter I'm thinking of...</h2>"
+	"<p> wins:" + wins + "</p>" +
+	"<p> losses:"+ losses + "</p>" +
+	"<p> Guess Left: " + guessLeft + "</p>" +
+	"<p> Your guesses so far: " + guessesSoFar + "</p>";
 
 	document.querySelector("#game").innerHTML = html;
-        }
-    };
+}
+    
